c632b4b09d4b33887e42bd6f18f96648a53d3d20
Emily Pearl <Emilypearl9193@gmail.com>
2024-12-12 22:07:01 -0700

    fetching and rendering api data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+  const getBurger = (async () => {
+		const response = await fetch('https://bobsburgers-api.herokuapp.com/burgerOfTheDay/20')
+    return await response.json()
+	})()
+
+
+</script>
+
+{#await getBurger}
+	<p>...waiting</p>
+{:then data}
+<p>{data.name}</p>
+<p>{data.price}$</p>
+<p>season {data.season}</p>
+<p>episode {data.episode}</p>
+    
+{:catch error}
+	<p>An error occurred!</p>
+{/await}
